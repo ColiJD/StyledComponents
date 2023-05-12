a56978befed9a01fd660aa85d77e247ea0d553fa
ColiJD <colindresj9@gmail.com>
2023-05-12 13:15:11 -0600

    Corrigiendo imagenes del tema

--- src/Components/SwitcherTema/index.js
+++ src/Components/SwitcherTema/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import themeOn from "../../assets/images/themeOn.svg"
+import themeOn from "../../assets/images/themeOn.svg";
 import themeOff from "../../assets/images/themeOff.svg";
 import { Icono } from "../../assets";
 
-export default (tema) => {
-    const claro = <Icono src={themeOn} alt="Tema Claro" />;
-    const oscuro = <Icono src={themeOff} alt="Tema Oscuro" />;
+export default ({ tema }) => {
+    const claro = <Icono src={themeOn} alt="Tema claro" />;
+    const oscuro = <Icono src={themeOff} alt="Tema oscuro" />;
 
     return <>{tema ? oscuro : claro}</>;
-}
+};
